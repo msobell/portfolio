0403fa0da681a34f3ed4bab404502df00f83a9f0
Max Sobell <max.sobell@cs.nyu.edu>
2010-12-04 17:40:09 -0500

    Fixed script (works for Gnome, sorry OS X :P )

--- multiplayersim.sh
+++ multiplayersim.sh
@@ -3,11 +3,11 @@ echo "Generating data.txt..."
 java -cp . DataGen 200 data.txt
 sleep 1
 echo "Spawning server..."
-gnome-terminal --geometry=100x100 --title="SERVER" -x java -cp . Simulator data.txt 10000 true
+gnome-terminal --geometry=100x30 --title="SERVER" -x java -cp . Simulator data.txt 10000 true
 sleep 5
 echo "Spawning random 1..."
-gnome-terminal --geometry=100x100 --title="RANDOM 1" -x java -cp . RandomPlayer localhost 10000 random1 200 5
+gnome-terminal --geometry=100x30 --title="RANDOM 1" -x java -cp . RandomPlayer localhost 10000 random1 200 5
 echo "Spawning random 2..."
-gnome-terminal --geometry=100x100 --title="RANDOM 2" -x java -cp . RandomPlayer localhost 10000 random2 200 5
+gnome-terminal --geometry=100x30 --title="RANDOM 2" -x java -cp . RandomPlayer localhost 10000 random2 200 5
 echo "Now hit play on the Portfolio Simulator window!"
 
--- multiplayersim200.sh
+++ multiplayersim200.sh
@@ -1,12 +1,11 @@
-java -cp . DataGen 200 data.txt &
-sleep 2
-java -cp . Simulator data.txt 10000 true &
+#! /bin/bash
+echo "Generating data.txt..."
+java -cp . DataGen 200 data.txt
+sleep 1
+echo "Spawning server..."
+gnome-terminal --geometry=100x30 --title="SERVER" -x java -cp . Simulator data.txt 10000 false 200
 sleep 5
-java -cp . RandomPlayer localhost 10000 rnd1 200 7 &
-java -cp . RandomPlayer localhost 10000 rnd2 200 7 &
-java -cp . RandomPlayer localhost 10000 rnd1 200 7 &
-java -cp . RandomPlayer localhost 10000 rnd2 200 7 &
-java -cp . RandomPlayer localhost 10000 rnd1 200 7 &
-java -cp . RandomPlayer localhost 10000 rnd2 200 7 &
-java -cp . RandomPlayer localhost 10000 rnd1 200 7 &
-java -cp . RandomPlayer localhost 10000 rnd2 200 7 &
+echo "Spawning random 1..."
+gnome-terminal --geometry=100x30 --title="RANDOM 1" -x java -cp . RandomPlayer localhost 10000 random1 200 200
+echo "Spawning random 2..."
+gnome-terminal --geometry=100x30 --title="RANDOM 2" -x java -cp . RandomPlayer localhost 10000 random2 200 200
